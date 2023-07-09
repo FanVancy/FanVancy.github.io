1182f1bfaceee1298eb4c3ecae308c93a9d3c52c
yinuo <139069906+FanVancy@users.noreply.github.com>
2023-07-10 01:42:30 +0800

    更新

---
+++ photo.js
@@ -0,0 +1,120 @@
+const photosInfo = [
+    {
+        cephalogram_url: './asset/img/Sagaya/1.jpg',
+        name: 'Vancys Living Memories ',
+        introduction: 'Memories washed away by time',
+        size: 'width-24',
+        children: [
+            {
+                children_url: './asset/img/Sagaya/2.jpg',
+                children_introduction: `An engaging platform in a “post Covid” world that allows guests to travel the globe and feel the
+                breadth of impact and services AWS has to offer. Through an inventive physical fabrication and
+                custom-engineered Hoverbar, we gave guests the power to dive deep into the AWS portfolio without
+                a touch.`,
+            },
+            {
+                children_url: './asset/img/Sagaya/3.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './asset/img/Sagaya/4.jpg',
+                children_introduction: `An engaging platform in a “post Covid” world that allows guests to travel the globe and feel the
+                breadth of impact and services AWS has to offer.`,
+            },
+            {
+                children_url: './asset/img/Sagaya/5.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './asset/img/Sagaya/6.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './asset/img/Sagaya/7.jpg',
+                children_introduction: `An engaging platform in a “post Covid” world that allows guests to travel the globe and feel the
+                breadth of impact and services AWS has to offer. `,
+            },
+        ]
+    },
+    {
+        cephalogram_url: './asset/img/Between_Sea_ Sky/0c28e4151624419.630f528f3eded.jpg',
+        name: 'Amazon Hoverbar Experience',
+        introduction: 'Spatial & Experience Design',
+        size: 'width-11',
+        children: [
+            {
+                children_url: './asset/img/Between_Sea_ Sky/af3dd8151624419.630f528f403bb.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './asset/img/Between_Sea_ Sky/b8fd0d151624419.630f528f3e60f.jpg',
+                children_introduction: `“理想主义的花最终会盛开在现实主义的土壤中，我的热情会在现实的平凡中发光发热`,
+            },
+            {
+                children_url: './asset/img/Between_Sea_ Sky/daf96a151624419.630f528f3d86f.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './asset/img/Between_Sea_ Sky/edff6b151624419.630f528f3df83.jpg',
+                children_introduction: `拥有的力量越多，能做到的事就更多。哪怕完成这些事需要代价，哪怕理想从来
+                都建立在痛苦之上，我也…不想后退。`,
+            },
+        ]
+    },
+    {
+        cephalogram_url: './asset/img/eye/0664e5108747605.5fc4f79d63203.jpg',
+        name: 'Amazon Hoverbar Experience',
+        introduction: 'Spatial & Experience Design',
+        size: 'width-24',
+        children: [
+            {
+                children_url: './asset/img/eye/3d5dff108747605.5fc4f79d64ee8.jpg',
+                children_introduction: `An engaging platform in a “post Covid” world that allows guests to travel the globe and feel the
+                breadth of impact and services AWS has to offer.`,
+            },
+            {
+                children_url: './asset/img/eye/79b86c108747605.5fc4f79d696d3.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './asset/img/eye/7ec023108747605.5fc4f79d677f6.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './asset/img/eye/9d889c108747605.5fc4f79d68951.jpg',
+                children_introduction: '',
+            },
+        ]
+    },
+    {
+        cephalogram_url: './asset/img/F1_Illustrations/13e0c5108324013.5fbb97f88211e.jpg',
+        name: 'Amazon Hoverbar Experience',
+        introduction: 'Spatial & Experience Design',
+        size: 'width-11',
+        children: [
+            {
+                children_url: './asset/img/F1_Illustrations/38571d108324013.5fbb99a68bb17.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './asset/img/F1_Illustrations/9a011a108324013.5fbb97f881332.jpg',
+                children_introduction: `An engaging platform in a “post Covid” world that allows guests to travel the globe and feel the
+                breadth of impact and services AWS has to offer.`,
+            },
+            {
+                children_url: './asset/img/F1_Illustrations/a80c35108324013.5fbb97f881912.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './asset/img/F1_Illustrations/f8c54f108324013.5fbb97f87ffe2.jpeg',
+                children_introduction: `An engaging platform in a “post Covid” world that allows guests to travel the globe and feel the
+                breadth of impact and services AWS has to offer.`,
+            },
+            {
+                children_url: './asset/img/F1_Illustrations/ff0b4d108324013.5fbb97f880a76.jpg',
+                children_introduction: '',
+            },
+        ]
+    },
+
+]
